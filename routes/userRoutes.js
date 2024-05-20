38a54fae66e8162f464b26d88e0a3149fe24ee9e
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-05-20 23:36:53 +0530

    User Routes added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,29 @@
 import express from "express";
-import {SignUp, login, myProfile } from '../controllers/userController.js'
+import {
+  SignUp,
+  acceptFriendRequest,
+  login,
+  myProfile,
+  searchUser,
+  sendFriendRequest,
+} from "../controllers/userController.js";
 import isAuthenticated from "../middlewares/auth.js";
 import { singleAvatar } from "../middlewares/multer.js";
 
-const router=express.Router();
+const router = express.Router();
 
 //tested routes
 
-router.route("/signup").post(singleAvatar,SignUp);
+router.route("/signup").post(singleAvatar, SignUp);
 router.route("/login").post(login);
-router.route("/me").get(isAuthenticated,myProfile);
 
+router.use(isAuthenticated);
+router.route("/me").get(myProfile);
 
+router.route("/search").get(searchUser);
 
+router.route("/sendrequest").put(sendFriendRequest);
 
+router.route("/acceptrequest").put(acceptFriendRequest);
 
 export default router;
