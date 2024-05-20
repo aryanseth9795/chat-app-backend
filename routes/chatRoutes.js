a5cdbf2ee0961d63c5b121be204ef4121fa5f22b
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-05-20 23:47:56 +0530

    ccokies option added

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -13,8 +13,6 @@ router.route("/mygroups").get(getMyGroups);
 router.route("/addmembers").put(addMembers);
 router.route("/removeMember").put(removeMember);
 router.route("/leave/:id").delete(leaveGroup);
-
-
 router.route("/message").post(attachmentsMulter,sendAttachments);
 router.route("/message/:id").get(getChatDetails);
 router.route("/:id").get(getMessages).put(renameGroup).delete(deleteChat);
