83a1a1765a77907e0f0d02fccbd5c265519d0c62
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-05-22 23:46:48 +0530

    comments removed

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,32 +1,4 @@
-// const uploadFilesToCloudinary = async (files = []) => {
-//     const uploadPromises = files.map((file) => {
-//       return new Promise((resolve, reject) => {
-//         cloudinary.uploader.upload(
-//           getBase64(file),
-//           {
-//             resource_type: "auto",
-//             public_id: uuid(),
-//           },
-//           (error, result) => {
-//             if (error) return reject(error);
-//             resolve(result);
-//           }
-//         );
-//       });
-//     });
 
-//     try {
-//       const results = await Promise.all(uploadPromises);
-
-//       const formattedResults = results.map((result) => ({
-//         public_id: result.public_id,
-//         url: result.secure_url,
-//       }));
-//       return formattedResults;
-//     } catch (err) {
-//       throw new Error("Error uploading files to cloudinary", err);
-//     }
-//   };
 
 import cloudinary from "cloudinary";
 import ErrorHandler from "./ErrorHandler";
