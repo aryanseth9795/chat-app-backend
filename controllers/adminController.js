d74075404a0c5c6c6a0fbb05e036f5a6504886dd
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2024-05-22 23:35:30 +0530

    admin controller added (users )

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -27,4 +27,50 @@ export const adminLogin=TryCatch(async(req,res,next)=>{
         success: true,
         message: "Authenticated Successfully, Welcome ADMIN",
       });
-})
+});
+
+const adminLogout = TryCatch(async (req, res, next) => {
+    return res
+      .status(200)
+      .cookie("chattu-admin-token", "", {
+        ...cookieOptions,
+        maxAge: 0,
+      })
+      .json({
+        success: true,
+        message: "Logged Out Successfully",
+      });
+  });
+  
+  export const getAdminData = TryCatch(async (req, res, next) => {
+    return res.status(200).json({
+      admin: true,
+    });
+  });
+  
+  export const allUsers = TryCatch(async (req, res) => {
+    const users = await User.find({});
+  
+    const transformedUsers = await Promise.all(
+      users.map(async ({ name, username, avatar, _id }) => {
+        const [groups, friends] = await Promise.all([
+          Chat.countDocuments({ groupChat: true, members: _id }),
+          Chat.countDocuments({ groupChat: false, members: _id }),
+        ]);
+  
+        return {
+          name,
+          username,
+          avatar: avatar.url,
+          _id,
+          groups,
+          friends,
+        };
+      })
+    );
+  
+    return res.status(200).json({
+      status: "success",
+      users: transformedUsers,
+    });
+  });
