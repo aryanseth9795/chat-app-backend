5ddb5bba3ffb70e62e0de19415441ab9a6baaa39
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2025-03-04 12:18:32 +0530

    cookie and jwt expire bug fixes

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -1,16 +1,12 @@
 import jwt from "jsonwebtoken";
 
-
-
-
-
 const cookieOptions = {
-  maxAge: process.env.JWT_EXPIRY * 24 * 60 * 60 * 1000 || 3 * 24 * 60 * 60 * 1000  ,
+  maxAge:
+    process.env.COOKIE_EXPIRY * 24 * 60 * 60 * 1000 || 3 * 24 * 60 * 60 * 1000,
   sameSite: "none",
   httpOnly: true,
   secure: true,
-};   
-
+};
 
 const sendToken = async (res, user, code, message) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
