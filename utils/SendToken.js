7458958152ce8bf5a0f4114b9a6977c27b663384
aryanseth9795 <99879650+aryanseth9795@users.noreply.github.com>
2025-03-04 12:08:05 +0530

    Cookies option added

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -1,11 +1,23 @@
 import jwt from "jsonwebtoken";
 
+
+
+
+
+const cookieOptions = {
+  maxAge: process.env.JWT_EXPIRY * 24 * 60 * 60 * 1000 || 3 * 24 * 60 * 60 * 1000  ,
+  sameSite: "none",
+  httpOnly: true,
+  secure: true,
+};   
+
+
 const sendToken = async (res, user, code, message) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRY,
   });
 
-  res.status(code).cookie("token", token).json({
+  res.status(code).cookie("token", token, cookieOptions).json({
     success: true,
     message,
   });
